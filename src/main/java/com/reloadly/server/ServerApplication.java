9c4c39926967653785a86068ae1dfb8c632f762b
Emili Garriga <porsilasmoscasweb@gmail.com>
2022-11-22 09:59:16 +0100

    some changes

--- src/main/java/com/reloadly/server/ServerApplication.java
+++ src/main/java/com/reloadly/server/ServerApplication.java
@@ -3,6 +3,7 @@ package com.reloadly.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class ServerApplication {
